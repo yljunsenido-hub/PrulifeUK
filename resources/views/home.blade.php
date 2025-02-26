b8acc7fa01f74c06a9045a34c6ae414c361d9ab4
johnjoven <plukjohnjovennapay@gmail.com>
2025-02-26 12:24:58 +0800

    web bg and form update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,14 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login/Register Page</title>
+    <title>BCQ AGENTS PORTAL</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
-<body class="bg-gray-100 flex flex-col min-h-screen">
+
+<body class="bg-gray-100 flex flex-col min-h-screen bg-cover bg-center bg-no-repeat" style="background-image: url('{{ asset('images/background.png')}}');">
+
 
     <div class="flex-grow flex items-center justify-center">
         <div class="bg-white shadow-md rounded-lg p-8 w-full max-w-sm">
-            <h2 class="text-2xl font-bold text-center mb-6">Login</h2>
+
+        <div class="flex justify-center mb-4">
+            <img src="images/bcdqlogo.png" alt="bcdq logo" class="w-30 h-40 rounded-lg">
+        </div>
+
+            <h2 class="text-2xl font-bold text-center mb-6">BLUE CHALCEDONY QUARTZ AGENTS PORTAL</h2>
             <form>
                 <div class="mb-4">
                     <label class="block text-gray-700 text-sm font-bold mb-2" for="email">Agent Code</label>
@@ -32,17 +39,18 @@
             </p>
         </div>
     </div>
+     
 
     <footer class="bg-gray-800 text-white py-4">
-        <div class="container mx-auto flex justify-between items-center">
+        <div class="container mx-auto flex justify-between items-center px-5">
             <div class="text-left">
-                <p>Left Text</p>
+                <p>©JYNStudent</p>
             </div>
-            <div class="text-center">
-                <p>Middle Text</p>
+            <div class="text-center" class="text-x0" class="font-mono">
+                <p></p>
             </div>
             <div class="text-right">
-                <p>Right Text</p>
+                <p>BCMS - Blue Chalcedony Management System</p>
             </div>
         </div>
     </footer>
