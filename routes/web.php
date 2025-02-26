a5bba9d959e3b6646653a70c3adc34c241621d72
JunellSasaki <Junellsasaki12345@gmail.com>
2025-02-26 16:54:25 +0800

    database update

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,8 @@ Route::get('/', function () {
     return view('login');
 });
 
+Route::get('/home', function () {
+    return view('home');
+});
+
 Route::get('/sampleDB', [AgentLoginController::class, 'index']);
