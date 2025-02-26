ea31892c7fda780266ffb770dca2c847ccc860a6
neilfranz <plukneilfranz@gmail.com>
2025-02-26 10:58:12 +0800

    footser check

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,27 +14,27 @@
             <form>
                 <div class="mb-4">
                     <label class="block text-gray-700 text-sm font-bold mb-2" for="email">Agent Code</label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email" required>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="Agent Code" required>
                 </div>
                 <div class="mb-6">
                     <label class="block text-gray-700 text-sm font-bold mb-2" for="password">Password</label>
                     <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password" required>
                 </div>
-                                <div class="flex justify-center">
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                <div class="flex justify-center">
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 w-full rounded focus:outline-none focus:shadow-outline" type="submit">
                         Login
                     </button>
                 </div>
-
             </form>
-            <p class="mt-4  text-gray-600 text-sm">
-            <a href="#" class="text-blue-500 hover:text-blue-700">Forgot Password?</a>
+            
+            <p class="mt-4 text-gray-600 text-sm text-center">
+                <a href="#" class="text-blue-500 hover:text-blue-700">Forgot Password?</a>
             </p>
         </div>
     </div>
 
-    <footer class="bg-gray-800 text-white text-center py-4">
-    <div class="container mx-auto flex justify-between items-center">
+    <footer class="bg-gray-800 text-white py-4">
+        <div class="container mx-auto flex justify-between items-center">
             <div class="text-left">
                 <p>Left Text</p>
             </div>
